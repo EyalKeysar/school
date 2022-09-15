58a6cfff20275ecd0cd0b1b778076b8d7eccb621
eyal_keysar <keysareyal@gmail.com>
2022-09-15 09:00:25 +0300

    wordcount

---
+++ cyber/homework/word_count/main.py
@@ -0,0 +1,19 @@
+import os
+
+main_dir = r"C:\Networks\school\cyber\homework\word_count"
+INPUT_PATH = main_dir + r"\input.txt"
+DICT_PATH = main_dir + r"\words.txt"
+OUTPUT_PATH = main_dir + r"\output.txt"
+
+file = open(INPUT_PATH, "r")
+words = file.read().split(" ") # Read file and split its words
+
+dictionary = open(DICT_PATH, "r")
+valid_words = dictionary.read().split("\n")
+
+def validate_word_list(words):
+    return [word for word in words if word in valid_words]
+    
+def write(correct_words):
+    f = open(OUTPUT_PATH, "w")
+    f.write("\n".join(correct_words))
