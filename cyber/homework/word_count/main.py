2ee3901c49180ace4374f2ab207214a3c36af71d
eyal_keysar <keysareyal@gmail.com>
2022-09-19 10:31:35 +0300

    start wordcount

--- cyber/homework/word_count/main.py
+++ cyber/homework/word_count/main.py
@@ -11,9 +11,19 @@ words = file.read().split(" ") # Read file and split its words
 dictionary = open(DICT_PATH, "r")
 valid_words = dictionary.read().split("\n")
 
+def get_word():
+    """
+    This function takes input from the user and returns it.
+
+    Returns:
+        String: The word taken from user.
+    """
+
+    return input("word : ")
+
 def validate_word_list(words):
     return [word for word in words if word in valid_words]
-    
+
 def write(correct_words):
     f = open(OUTPUT_PATH, "w")
     f.write("\n".join(correct_words))
