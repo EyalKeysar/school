b96cc317c47fad102e40f41d17213e8c2bd22137
eyalKey <eyal@some.com>
2022-10-02 00:14:18 +0300

    modified and comments

--- cyber/homework/6# LogPuzzle/logpuzzle.py
+++ cyber/homework/6# LogPuzzle/logpuzzle.py
@@ -1,3 +1,8 @@
+"""
+Logpuzzle exercise
+Given an apache logfile, find the puzzle urls and download the images.
+"""
+
 #!/usr/bin/python
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -7,31 +12,28 @@
 # http://code.google.com/edu/languages/google-python-class/
 
 import os
-import re
 import sys
 import urllib
 import urllib.request
-
-"""Logpuzzle exercise
-Given an apache logfile, find the puzzle urls and download the images.
-
-Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
-"""
-
 
 def read_urls(filename: str) -> list:
     """Returns a list of the puzzle urls from the given log file,
     extracting the hostname from the filename itself.
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
+    if("message" in filename):
+        print("The program will download the Message")
+    elif("logo" in filename):
+        print("The program will download the Logo")
+    else:
+        print("The program will download Unknown")
+
     try:
         txt = open(filename, "r").read()  # Read text from file
 
     except FileNotFoundError:
         print("File not found")
         sys.exit(1)  # Exit program.
-
     except Exception as err:
         print("Untracked: " + err)
         sys.exit(1)  # Exit program.
@@ -52,56 +54,59 @@ def read_urls(filename: str) -> list:
 
         except IndexError:
             print(f"Index error:\nwords = {words}")
+            sys.exit(1)
         except Exception as err:
-            print(f"Untracked: {err}")
+            print(f"Untracked: {err}\nin read_urls function")
+            sys.exit(1)
     # Change the type of the list to a set and then back to list that so it will delete duplicates.
     files_names = list(set(files_names))
     # Sort list by the abc.
-    
+
     files_names.sort(key=lambda x: x[len(x)-8:])
 
     return files_names
 
 
 def download_images(img_urls: list, dest_dir: str) -> None:
-    """Given the urls already in the correct order, downloads
+    """
+    Given the urls already in the correct order, downloads
     each image into the given directory.
     Gives the images local filenames img0, img1, and so on.
     Creates an index.html in the directory
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
-    html_to_write = "<html><body>"
-    for file in img_urls:
-        url_to_get = r"https://data.cyber.org.il/python/logpuzzle/" + file
-        data = urllib.request.urlretrieve(url_to_get, file)
-        os.replace(file, dest_dir + file)
-        html_to_write += "<img src = \"" + dest_dir + file + "\">"
-    html_to_write += "</body></html>"
-    html_file = open("output.html", "w").write(html_to_write)
+    html_to_write = "<html><body>"  # Start of an html file.
+    sum_of_images = 0
+    try:
+        for file in img_urls:
+            url_to_get = r"https://data.cyber.org.il/python/logpuzzle/" + file
+            data = urllib.request.urlretrieve(url_to_get, file)  # Get image from current url.
 
-# 10.254.254.65 - - [06/Aug/2007:00:09:21 -0700] "GET /python/logpuzzle/p-bfhh-bahj.jpg HTTP/1.0" 302 528 "-" "googlebot-mscrawl-moma (enterprise; bar-XYZ; foo123@facebook.com)"
+            # The image will be imported to the program directory
+            # so it need to be replaced to dest_dir.
+            os.replace(file, dest_dir + file)
+
+            # Add img src to the html page.
+            html_to_write += "<img src = \"" + dest_dir + file + "\">"
+            sum_of_images += 1
+            print(f"Image number: {sum_of_images} has been successfully downloaded.")
+
+    except Exception as err:
+        print(f"Untracked: {err}\nin download_images function")
+        sys.exit(1)
+
+    html_to_write += "</body></html>"  # End of an html file.
+    open("output.html", "w").write(html_to_write)  # Write to the html file.
+
 def main() -> None:
-    img_urls = read_urls(r"./logo_data.cyber.org.il")
+    """
+    This is the main function.
+    """
+    img_urls = read_urls(r"./message_data.cyber.org.il")
     dest_dir = "./images/"
     download_images(img_urls, dest_dir)
     sys.exit(0)
-    args = sys.argv[1:]
-
-    if not args:
-        print('usage: [--todir dir] logfile ')
-        sys.exit(1)
-
-    todir = ''
-    if args[0] == '--todir':
-        todir = args[1]
-        del args[:2]
-    img_urls = read_urls(args[0])
-    
-    if todir:
-        download_images(img_urls, todir)
-    else:
-        print('\n'.join(img_urls))
 
 if __name__ == '__main__':
     main()
