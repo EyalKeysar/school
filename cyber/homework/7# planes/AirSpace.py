6913bee5c62311ddb8026bbfe61125ebb03397a0
EyalKeysar <104645207+EyalKeysar@users.noreply.github.com>
2022-10-06 20:59:45 +0300

    asd

--- cyber/homework/7# planes/AirSpace.py
+++ cyber/homework/7# planes/AirSpace.py
@@ -5,13 +5,13 @@ from typing import *
 class AirSpace:
     
     def __init__(self):
-        self.__airGrid: List[AirCube] = []
+        self.__airGrid: List[List[AirCube]] = [[]]
         self.__crazyPlanes: List[CrazyPlane] = []
 
 
         for i in range(10):
             for r in range(10):
-                self.__airGrid[i, r] = AirCube(i, r)
+                self.__airGrid[i][r] = AirCube(i, r)
         for i in range(4):
             self.__crazyPlanes[i] = CrazyPlane()
             
--- cyber/homework/7# planes/CrazyPlane.py
+++ cyber/homework/7# planes/CrazyPlane.py
@@ -3,6 +3,21 @@ import random
 
 class CrazyPlane:
     def __init__(self):
-        
+        tempX = random.randint(1, 8)
+        tempY = random.randint(1, 8)
+        self.__pos = AirCube(tempX, tempY)
+        self.__velX = random.randint(0, 2)
+        if(self.__velX == 0):
+            self.__velY = random.randint(1, 2)
+        else:
+            self.__velY = random.randint(0, 2)
+
     def update(self) -> None:
         print()
+    
+    def getNewVel(self) -> tuple:
+        self.__velX = random.randint(-1, 1)
+        if(self.__velX == 0):
+            self.__velY = random.randint(0, 1)
+        else:
+            self.__velY = random.randint(0, 2)
