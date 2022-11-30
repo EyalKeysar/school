853aa6b735680600d4116b8b6f4a5c98d4c9177d
eyal_keysar <keysareyal@gmail.com>
2022-11-30 16:15:35 +0200

    comsci toresin calss

---
+++ comsci/homework/Tores and classes/ConsoleApp1/ConsoleApp1/Bank.cs
@@ -0,0 +1,163 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.CollectionsLib;
+
+namespace ConsoleApp1
+{
+    class Bank
+    {
+        /**
+         * Q1:
+         *  The name of the bank is a string, self explanatory.
+         *  The transaction is a queue of integers, its the only data structure we have learned.
+         */
+        private string name;
+        private Queue<int> transaction;
+        /**
+         * Set name.
+         * O(1).
+         * (** O(n) if we represent the name as array of char like in c ).
+         */
+        public Bank(string name)
+        {
+            this.name = name;
+            this.transaction = new Queue<int>();
+        }
+
+        /**
+         * Gets name of bank.
+         * O(1).
+         */
+        public string GetName() { return this.name; }
+
+        /**
+         * Adds transaction to the transaction queue of the bank.
+         * O(1).
+         */
+        public void AddTransaction(int num)
+        {
+            transaction.Insert(num);
+        }
+
+        /**
+         * Return thae total amount of money in the bank.
+         * O(n).
+         */
+        public int TotalAmount()
+        {
+            int sum = 0;
+            Queue<int> q2 = new Queue<int>();
+            while (!this.transaction.IsEmpty())
+            {
+                int curVal = this.transaction.Remove();
+                sum += curVal;
+                q2.Insert(curVal);
+            }
+            while (!q2.IsEmpty())
+            {
+                this.transaction.Insert(q2.Remove());
+            }
+            return sum;
+        }
+        
+        /**
+         * Return how many transation the bank committed.
+         * O(n).
+         */
+        public int worked()
+        {
+            int sum = 0;
+            Queue<int> q2 = new Queue<int>();
+            while (!this.transaction.IsEmpty())
+            {
+                int curVal = this.transaction.Remove();
+                sum++;
+                q2.Insert(curVal);
+            }
+            while (!q2.IsEmpty())
+            {
+
+                this.transaction.Insert(q2.Remove());
+            }
+            return sum;
+        }
+        
+        /**
+         * Return the last transaction the bank has committed.
+         * O(n).
+         */
+        public int LastTransaction()
+        {
+            int val = 0;
+            Queue<int> q2 = new Queue<int>();
+            while (!this.transaction.IsEmpty())
+            {
+                int curVal = this.transaction.Remove();
+                q2.Insert(curVal);
+            }
+            while (!q2.IsEmpty())
+            {
+                val = q2.Remove();
+                this.transaction.Insert(val);
+
+            }
+            return val;
+        }
+
+        /**
+         * Return if the bank has committed more positive transaction or
+         * more negative transaction.
+         * O(n).
+         */
+        public int MoreOrLess()
+        {
+            int sum = 0;
+            Queue<int> q2 = new Queue<int>();
+            while (!this.transaction.IsEmpty())
+            {
+                int curVal = this.transaction.Remove();
+                if(curVal > 0)
+                {
+                    sum++;
+                }
+                if(curVal < 0)
+                {
+                    sum = sum - 1;
+                }
+                q2.Insert(curVal);
+            }
+            while (!q2.IsEmpty())
+            {
+                this.transaction.Insert(q2.Remove());
+            }
+            if(sum > 0) { return 1; }
+            else if(sum == 0) { return 0; }
+            else { return -1; }
+        }
+
+        /**
+         * Prints bank transaction history.
+         * O(n).
+         */
+        public void History()
+        {
+            Console.WriteLine("Bank Name:" + this.GetName());
+            int val = 0;
+            Queue<int> q2 = new Queue<int>();
+            while (!this.transaction.IsEmpty())
+            {
+                val++;
+                int curVal = this.transaction.Remove();
+                Console.WriteLine("transaction num" + val + ":" + curVal);
+                q2.Insert(curVal);
+            }
+            while (!q2.IsEmpty())
+            {
+                this.transaction.Insert(q2.Remove());
+            }
+        }
+    }
+}
---
+++ comsci/homework/Tores and classes/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.CollectionsLib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b1 = new Bank("BankOne");
+
+            b1.AddTransaction(101);
+            b1.AddTransaction(-19);
+            b1.AddTransaction(-100);
+            b1.AddTransaction(20);
+            b1.AddTransaction(20);
+
+            Console.WriteLine("cur amount = " + b1.TotalAmount());
+            Console.WriteLine("cur num of transaction: " + b1.worked());
+            Console.WriteLine("last transaction : " + b1.LastTransaction());
+
+
+            Bank b2 = new Bank("BankSecond");
+            b2.AddTransaction(53);
+            b2.AddTransaction(-341);
+            b2.AddTransaction(-12);
+            b2.AddTransaction(2231);
+
+            if (b1.TotalAmount() > b2.TotalAmount()) { Console.WriteLine(b1.GetName()); }
+            else { Console.WriteLine(b2.GetName()); }
+
+            if (b1.worked() > b2.worked()) { Console.WriteLine(b1.GetName()); }
+            else { Console.WriteLine(b2.GetName()); }
+
+            if (b1.GetName().Length > b2.GetName().Length) { Console.WriteLine(b1.GetName()); }
+            else { Console.WriteLine(b2.GetName()); }
+
+            Bank b3 = new Bank("BankThree");
+            b3.AddTransaction(5312);
+            b3.AddTransaction(-3421);
+            b3.AddTransaction(123);
+
+            Bank[] bArr = new Bank[5];
+            bArr[0] = b1;
+            bArr[1] = b2;
+            bArr[2] = b3;
+            // 2 pointers to each bank, one when I created it and one from thae array.
+            bArr[3] = new Bank("BankFour");
+            // 1 pointer because the bank created in the array pointer. when we created array of bankes it allocated space in heap for the banks,
+
+            string longestName = "";
+            int longestNameInd = -1;
+            int richestSum = int.MinValue; int richestInd = -1;
+            for (int i = 0; i < bArr.Length; i++)
+            {
+                if(bArr[i] != null)
+                {
+                    if (bArr[i].GetName().Length > longestName.Length)
+                    {
+                        longestName = bArr[i].GetName();
+                        longestNameInd = i;
+                    }
+                    if (bArr[i].TotalAmount() > richestSum)
+                    {
+                        richestInd = i;
+                        richestSum = bArr[i].TotalAmount();
+                    }
+                    if (bArr[i].MoreOrLess() == 1)
+                    {
+                        Console.WriteLine("more positive transaction: " + bArr[i].GetName());
+                    }
+                }
+ 
+            }
+            if(longestNameInd >= 0)
+            {
+                Console.WriteLine("longest name: " + bArr[longestNameInd].GetName());
+            }
+            if(richestInd >= 0)
+            {
+                Console.WriteLine("richest name " + bArr[richestInd].GetName());
+            }
+            Console.ReadLine();
+        }
+    }
+}
