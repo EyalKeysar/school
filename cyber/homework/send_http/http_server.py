23f6b1a52411364c218d10b7a3e5fc9d68da1b34
eyalKey <eyal@some.com>
2022-12-03 00:37:39 +0200

    sad

--- cyber/homework/send_http/http_server.py
+++ cyber/homework/send_http/http_server.py
@@ -12,7 +12,8 @@ import os
 # TO DO: set constants
 IP = '0.0.0.0'
 PORT = 80
-SOCKET_TIMEOUT = 0.1
+SOCKET_TIMEOUT = 5
+REDIRECTION_DICTIONARY = {'/source.html':'http://webroot/imgs/abstract.jpg'}
 ROOT = "./webroot"
 HTML_PATH = "./webroot/index.html"
 
@@ -22,16 +23,23 @@ HTML_PATH = "./webroot/index.html"
 def get_file_data(filename):
     """ Get data from file """
     try:
+        file_pointer = None
         # reading the file data.
         if(filename == "/"):
-            file_pointer = open(HTML_PATH, "r", encoding='utf-8')
+            file_pointer = open(HTML_PATH, "r", encoding = 'unicode_escape')
         else:
-            file_pointer = open(ROOT + filename, "r", encoding='utf-8')
+            for root, dirs, files in os.walk(r'C:\Networks\school\cyber\homework\send_http\webroot'):
+                for name in files:
+                    if name == filename: 
+                        file_pointer = open(os.path.abspath(os.path.join(root, name)), "r", encoding = 'unicode_escape')
+            if(not file_pointer):
+                file_pointer = open(HTML_PATH, "r", encoding = 'unicode_escape')
+                        
         file_data = file_pointer.read()
         file_pointer.close()
     except FileNotFoundError:
-        print("file not found")
-        sys.exit(0)
+        print("file not found 404 = " + filename)
+        file_data = None
     return file_data
 # --------------------------------------------------------------------
 
@@ -42,8 +50,9 @@ def validate_http_request(request):
     # Length validation of request.
     if(request[0:4] == "GET "):
         if(r"HTTP/1.1" in request):
-            current_path = "./webroot" + request[4:request.index(r" HTTP")]
-            return True, current_path.replace('/', '\\')
+            current_path = request[4:request.index(r" HTTP")] 
+            return True, current_path
+        
         else:
             return False, ""
     else:
@@ -55,41 +64,49 @@ def validate_http_request(request):
 def handle_client_request(resource, client_socket):
     """ Check the required resource, generate proper HTTP response and send to client"""
     # TO DO : add code that given a resource (URL and parameters) generates the proper response
-    
-    if resource == '':
-        url = DEFAULT_URL
-    else:
-        url = resource
 
     # TO DO: check if URL had been redirected, not available or other error code. For example:
-    if url in REDIRECTION_DICTIONARY:
+    if resource in REDIRECTION_DICTIONARY:
         # TO DO: send 302 redirection response
+        http_header = 'HTTP/1.1 302 Found\r\n'
+    else:
+        http_header = 'HTTP/1.1 200 OK\r\n'
 
     # TO DO: extract requested file type from URL (html, jpg etc)
+    filetype = resource.split('.')[-1]
     if filetype == 'html':
-        http_header = # TO DO: generate proper HTTP header
+        http_header += "Content-Type: text/html\r\n"# TO DO: generate proper HTTP header
     elif filetype == 'jpg':
-        http_header = # TO DO: generate proper jpg header
+        http_header += "Content-Type: image/jpg\r\n"# TO DO: generate proper jpg header
     # TO DO: handle all other headers
 
-    # TO DO: read the data from the file
-    data = get_file_data(filename)
-    http_response = http_header + data
+
+    data = get_file_data(resource.replace('/', ''))
+    if(data != None):
+        length = len(data)
+    else:
+        length = 0
+    http_header += 'content-length:' + str(length) + '\r\n\r\n'
+    if(data != None):
+        http_response = http_header + data
+    else:
+        http_response = http_header
     client_socket.send(http_response.encode())
 
+
 def handle_client(client_socket):
+    print("4")
     """ Handles client requests: verifies client's requests are legal HTTP, calls function to handle the requests """
     print('Client connected')
     while True:
         # TO DO: insert code that receives client request
         client_request = client_socket.recv(1024).decode()
+        print(client_request)
         # ...
-        
         valid_http, resource = validate_http_request(client_request)
         if valid_http:
             print('Got a valid HTTP request')
             handle_client_request(resource, client_socket)
-            client_socket.send(FIXED_RESPONSE.encode())
             break
         else:
             print('Error: Not a valid HTTP request')
@@ -108,11 +125,8 @@ def main():
     while True:
         client_socket, client_address = server_socket.accept()
         print('New connection received')
-        client_request = client_socket.recv(1024).decode()
-        boole, strinh = validate_http_request(client_request)
-        print("bool = " + str(boole) + "\nstring " + strinh)
-        client_socket.settimeout(SOCKET_TIMEOUT)
-        #handle_client(client_socket)
+        #client_socket.settimeout(SOCKET_TIMEOUT)
+        handle_client(client_socket)
 
 
 if __name__ == "__main__":
