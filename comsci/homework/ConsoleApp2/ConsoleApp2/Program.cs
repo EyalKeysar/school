d1febe6311c7092178768c9c1925e3060bdf1c84
EyalKeysar <eyal@some.com>
2023-07-08 21:52:38 +0300

    added readme

---
+++ comsci/homework/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static void PrintPyramid(int num)
+        {
+            /*
+             * Prints a star pyramid by length num.
+             */
+            for(int i = 0; i < num; i++)
+            {
+                for(int j = 0; j < (int) (num) - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool AllExists(int[] arr, int num)
+        {
+            bool[] boolArr = new bool[num + 1];
+            for (int i = 0; i < num + 1; i++)
+            {
+                boolArr[i] = false;
+            }
+
+            for(int i=0; i < arr.Length; i++)
+            {
+                if (arr[i] < num + 1 && arr[i] > 0)
+                {
+                    boolArr[arr[i]] = true;
+                }
+            }
+            for (int i = 1; i < num + 1; i++)
+            {
+                if (boolArr[i] == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static int HowManySubStrings(string mainString, string secondString)
+        {
+            int sum = 0;
+            for(int i = 0; i < mainString.Length;i++)
+            {
+                if(i + secondString.Length < mainString.Length + 1)
+                {
+
+                    bool isTrue = true;
+                    for (int j = 0; j < secondString.Length; j++)
+                    {
+                        if (mainString[i + j] != secondString[j])
+                        {
+                            isTrue = false;
+                        }
+                    }
+                    if (isTrue)
+                    {
+                        sum++;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ comsci/homework/ConsoleApp3/ConsoleApp3/MyQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class MyQueue
+    {
+        private int[] items;
+        private int front;
+        private int rear;
+        private int size;
+
+        public MyQueue(int capacity)
+        {
+            items = new int[capacity];
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (rear == items.Length - 1)
+            {
+                Console.WriteLine("Queue is full");
+                return;
+            }
+            rear++;
+            items[rear] = item;
+            size++;
+        }
+
+        public int Dequeue()
+        {
+            if (size == 0)
+            {
+                Console.WriteLine("Queue is empty");
+                return -1;
+            }
+            int item = items[front];
+            front++;
+            size--;
+            return item;
+        }
+    }
+}
